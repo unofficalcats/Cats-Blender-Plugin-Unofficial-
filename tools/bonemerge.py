f23bcdf21f220b7ee9bf91b86caf229c8a09b97c
Yusarina <hello@yusarina.xyz>
2025-01-31 04:39:58 +0000

    Revert Bone merge - Messed it up

--- tools/bonemerge.py
+++ tools/bonemerge.py
@@ -26,6 +26,7 @@ class LoadBonesButton(bpy.types.Operator):
         globs.root_bones_choices = {}
         choices = get_parent_root_bones(self, context)
         
+        # Use the wrap_dynamic_enum_items helper
         wrapped_items = Common.wrap_dynamic_enum_items(
             lambda s, c: choices,
             'merge_bone'
@@ -38,6 +39,7 @@ class LoadBonesButton(bpy.types.Operator):
         )
 
         return {'FINISHED'}
+
 
 @register_wrap
 class BoneMergeButton(bpy.types.Operator):
@@ -54,18 +56,17 @@ class BoneMergeButton(bpy.types.Operator):
     def execute(self, context):
         saved_data = Common.SavedData()
         armature = Common.set_default_stage()
-        
-        # Cache bones dictionary
-        bones_dict = {bone.name: bone for bone in armature.data.bones}
-        
+
         parent_bones = globs.root_bones[context.scene.merge_bone]
         mesh = Common.get_objects()[context.scene.merge_mesh]
         ratio = context.scene.merge_ratio
+        # debug
+        print(ratio)
 
         did = 0
         todo = 0
         for bone_name in parent_bones:
-            bone = bones_dict.get(bone_name)
+            bone = armature.data.bones.get(bone_name)
             if not bone:
                 continue
 
@@ -75,64 +76,72 @@ class BoneMergeButton(bpy.types.Operator):
         wm = bpy.context.window_manager
         wm.progress_begin(did, todo)
 
-        # Process bones using cached dictionary
+        # Start the bone check for every parent
         for bone_name in parent_bones:
             print('\nPARENT: ' + bone_name)
-            bone = bones_dict.get(bone_name)
+            bone = armature.data.bones.get(bone_name)
             if not bone:
                 continue
 
-            children = [child.name for child in bone.children]
+            children = []
+            for child in bone.children:
+                children.append(child.name)
 
             for child_name in children:
-                child = bones_dict.get(child_name)
+                child = armature.data.bones.get(child_name)
                 print('CHILD: ' + child.name)
-                self.check_bone(mesh, child, ratio, ratio, bones_dict)
+                self.check_bone(mesh, child, ratio, ratio)
                 did += 1
                 wm.progress_update(did)
 
         saved_data.load()
+
         wm.progress_end()
-        
         self.report({'INFO'}, t('BoneMergeButton.success'))
         return {'FINISHED'}
 
-    def check_bone(self, mesh, bone, ratio, i, bones_dict):
+    # Go through this until the last child is reached
+    def check_bone(self, mesh, bone, ratio, i):
         if bone is None:
             print('END FOUND')
             return
 
+        # Increase number by the ratio
         i += ratio
         bone_name = bone.name
 
-        # Collect children names
-        children = [child.name for child in bone.children]
-        
-        # Collect vertex groups for batched processing
-        mix_operations = []
+        # Get all children names
+        children = []
+        for child in bone.children:
+            children.append(child.name)
 
+        # Check if bone will be merged
         if i >= 100:
             i -= 100
+
             if bone.parent is not None:
                 parent_name = bone.parent.name
+
+                print('Merging ' + bone_name + ' into ' + parent_name+ ' with ratio ' + str(i))
+
+                # Mix the weights
+                Common.set_default_stage()
+                Common.remove_rigidbodies_global()
+                Common.set_active(mesh)
+
                 vg = mesh.vertex_groups.get(bone_name)
                 vg2 = mesh.vertex_groups.get(parent_name)
-                
                 if vg is not None and vg2 is not None:
-                    mix_operations.append((bone_name, parent_name))
+                    Common.mix_weights(mesh, bone_name, parent_name)
 
-        # Batch process vertex group operations
-        if mix_operations:
-            Common.set_default_stage()
-            Common.remove_rigidbodies_global()
-            Common.set_active(mesh)
-            
-            for bone_name, parent_name in mix_operations:
-                Common.mix_weights(mesh, bone_name, parent_name)
+                Common.set_default_stage()
+                Common.remove_rigidbodies_global()
+
+                # We are done, remove the bone
                 Common.remove_bone(bone_name)
 
-        # Process children using cached bones
-        for child_name in children:
-            child_bone = bones_dict.get(child_name)
-            if child_bone is not None:
-                self.check_bone(mesh, child_bone, ratio, i, bones_dict)
+        armature = Common.set_default_stage()
+        for child in children:
+            bone = armature.data.bones.get(child)
+            if bone is not None:
+                self.check_bone(mesh, bone, ratio, i)
